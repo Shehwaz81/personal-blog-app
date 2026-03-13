2883cbf14f24f95f1e66561fecda07c44217d22b
Shehwaz Saini <shehwazlongia@gmail.com>
2026-03-13 19:56:59 -0400

    connect sqlalchemy and add 'create article' functionality

--- main.py
+++ main.py
@@ -1,7 +1,49 @@
-from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, render_template, request, redirect, url_for
+
+from sqlalchemy.sql import func
 
 app = Flask(__name__)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TACK_MODIFICATIONS'] = False # disable for less memory use
+
+db = SQLAlchemy(app)
+
+# declare a table in database.db by making a model via inheritance of the db.Model class
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    article_title = db.Column(db.String(100), nullable=False)
+
+    article = db.Column(db.Text) # will be the content of the article (raw markdown string) t
+
+    def __repr__(self):
+        return f'<Article: {self.article_title}>'
+
 @app.route("/")
 def main():
-    return render_template("index.html")
+    articles = Article.query.order_by(Article.id.desc()).all()
+    return render_template('index.html', articles=articles)
+
+
+@app.route("/create", methods=["GET", "POST"])
+def create():
+    if request.method == "GET": # just load the create page
+        return render_template('create.html')
+    
+    elif request.method == "POST":
+        title = request.form.get("title")
+        article = request.form.get("article")
+        data = Article(article_title=title, article=article)
+        db.session.add(data)
+        db.session.commit()
+        return redirect(url_for('main'))
+
+@app.route("/view")
+def view():
+    return render_template('visit.html')
+
+    
